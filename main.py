6c6029f8e371fa53e100beff64196e4a2ff1a750
YGS <gsryu99@gmail.com>
2026-01-07 23:25:23 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget,     # 애플르케이션 핸들러와 빈 GUI 위젯
                              QPushButton, QVBoxLayout, QMessageBox,  # QMessageBox : 메시지 박스 위젯
-                             QPlainTextEdit)            # QPlainTextEdit 추가
+                             QPlainTextEdit,            # QPlainTextEdit 추가
+                             QHBoxLayout)               # QHBoxLayout 추가
 
 from PyQt5.QtGui import QIcon                           # icon을 추가하기 위한 라이브러리
 
@@ -20,10 +21,19 @@ class Calculator(QWidget):      # QWidget 클래스를 상속받아서 클래스
         self.btn1 = QPushButton('Message', self)    # 버튼 추가
         self.btn1.clicked.connect(self.activateMessage)     # 버튼 츨릭 시 핸들러 함수 연결
 
+        self.btn2 = QPushButton('Clear', self)      # 버튼 2 추가
+        self.btn2.clicked.connect(self.clearMessage)        # 버튼 2 핸들러 함수 연결
+
+        hbox = QHBoxLayout()        # 수평 박스 레이아웃을 추가하고, 버튼 1, 2 추가
+        hbox.addStretch(1)          # 공백
+        hbox.addWidget(self.btn1)   # 버튼 1 배치
+        hbox.addWidget(self.btn2)   # 버튼 2 배치
+
         vbox = QVBoxLayout()        # 수직 레이아웃 위젯 생성
         vbox.addWidget(self.text1)  # 수직 레이아웃에 텍스트 에디트 추가
         vbox.addStretch(1)          # 빈공간
-        vbox.addWidget(self.btn1)   # 버튼 위치
+        # vbox.addWidget(self.btn1)   # 버튼 위치
+        vbox.addLayout(hbox)        # btn1 위치에 hbox를 배치
         vbox.addStretch(1)          # 빈공간
 
         self.setLayout(vbox)    # 빈공간 - 버튼 - 빈공간 순으로 수직 배치된 레이아웃 설정
@@ -38,6 +48,8 @@ class Calculator(QWidget):      # QWidget 클래스를 상속받아서 클래스
         # QMessageBox.information(self, "information", "Button clicked!")
         self.text1.appendPlainText("Button clicked!")
 
+    def clearMessage(self):     # 버튼 2 핸들러 함수 정의
+        self.text1.clear()
 
 if __name__=='__main__':            # pyqt는 애플리케이션 당 1개의 QApplication이 필요함
     app = QApplication(sys.argv)    # QApplication 인스턴스 생성
